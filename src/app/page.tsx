78f107d6986e63ceaf97b77436d87c18792f4a89
ginabeki <ginabeki3@gmail.com>
2024-09-04 16:11:42 +0300

    Initialize project with next, react toolkit, and shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-600 font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
