770cb942e29a46a39a2e8c6bb8d52fc551444976
Chrissiku <chrissiku5@gmail.com>
2024-09-05 22:10:00 +0200

    Implement Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,21 +1,32 @@
+"use client";
+
 import { images } from "@/data";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import CustomImage from "../CustomImage";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const navigations = [
-  { name: "about", href: "/about" },
-  { name: "send money", href: "/send-money" },
-  { name: "marketplace", href: "/marketplace" },
-  { name: "features", href: "/features" },
-  { name: "contact", href: "/contact" },
+  { name: "about", href: "/about", customClass: "" },
+  {
+    name: "send money",
+    href: "/send-money",
+    customClass: "block lg:hidden xl:block",
+  },
+  { name: "marketplace", href: "/marketplace", customClass: "" },
+  { name: "features", href: "/features", customClass: "" },
+  { name: "contact", href: "/contact", customClass: "" },
 ];
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="w-full mx-auto">
-      <div className="w-full mx-auto flex flex-row items-center justify-between max-w-[1440px] p-5 md:px-10 lg:px-20">
-        <Link href={'/'} className="w-[200px]">
+      <div className="w-full mx-auto flex flex-row items-center justify-between max-w-[1440px] p-5 md:px-10">
+        <Link href={"/"} className="w-[200px]">
           <CustomImage
             src={images.logo2}
             alt="handShake Wallet logo"
@@ -24,26 +35,89 @@ const Navbar = () => {
             className="w-full h-full object-center object-cover"
           />
         </Link>
-        <nav className="inline-flex items-center justify-center space-x-5 text-[14px]">
+        <nav className="hidden lg:inline-flex items-center justify-center space-x-3 text-[14px]">
           {navigations.map((nav, index) => (
             <Link
               key={index}
               href={nav.href}
-              className="px-6 py-1.5 capitalize hover:bg-primary-yellow rounded-full text-center text-black flex flex-row items-center justify-center font-medium transition-all duration-200 ease-linear"
+              className={`px-6 py-1.5 capitalize rounded-full text-center text-black flex flex-row items-center justify-center font-medium transition-all duration-200 ease-linear ${
+                nav.customClass
+              } 
+              ${
+                String(pathname) === nav.href
+                  ? "bg-primary-yellow"
+                  : "bg-transparent hover:bg-primary-yellow/70"
+              }`}
             >
               <span>{nav.name}</span>
             </Link>
           ))}
         </nav>
-        <div className="text-[14px]">
-          <Link
-            href={"/send-money"}
-            className="px-6 py-2 capitalize bg-black text-white hover:text-black hover:bg-primary-yellow rounded-full text-center flex flex-row items-center justify-center font-medium transition-all duration-200 ease-linear"
+        <div className="inline-flex items-center justify-end space-x-10">
+          <div className="text-[14px] hidden md:block">
+            <Link
+              href={"/send-money"}
+              className={`px-6 py-2 capitalize rounded-full text-center flex flex-row items-center justify-center font-medium transition-all duration-200 ease-linear  ${
+                String(pathname) === "/send-money"
+                  ? "bg-primary-yellow text-black"
+                  : "bg-black hover:bg-primary-yellow/70 hover:text-black text-white"
+              }`}
+            >
+              <span>Send money</span>
+            </Link>
+          </div>
+          <button
+            type="button"
+            onClick={() => setIsOpen(!isOpen)}
+            className="inline-flex items-center justify-center lg:hidden"
           >
-            <span>Send money</span>
-          </Link>
+            <span className="sr-only">Open main menu</span>
+            {isOpen ? (
+              <XMarkIcon
+                aria-hidden="true"
+                className="h-8 w-8 text-red-600 group-data-[open]:block"
+              />
+            ) : (
+              <Bars3Icon
+                aria-hidden="true"
+                className="h-8 w-8 text-black group-data-[open]:hidden"
+              />
+            )}
+          </button>
         </div>
       </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="relative md:absolute right-0 w-full h-full backdrop-blur-md bg-black/5 p-5 overflow-hidden lg:hidden 
+            "
+          >
+            <div className="flex flex-col items-center justify-center gap-2 md:max-w-[300px] md:ml-auto w-full">
+              {navigations.map((nav) => (
+                <Link
+                  onClick={() => setIsOpen(!isOpen)}
+                  key={nav.name}
+                  href={nav.href}
+                  className={`capitalize relative w-full border-black p-3 inline-flex items-center justify-start space-x-5 rounded-lg ${
+                    nav.customClass
+                  }
+                   ${
+                     String(pathname) === nav.href
+                       ? "bg-primary-yellow"
+                       : "bg-primary-gray/20 hover:bg-primary-yellow/70 text-black"
+                   }`}
+                >
+                  <span>{nav.name}</span>
+                </Link>
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
