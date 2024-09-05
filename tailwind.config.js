e4903c4fa484b1e837671a6894090ea7b0bb0276
ginabeki <ginabeki3@gmail.com>
2024-09-06 00:17:07 +0300

    Update tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,43 @@ module.exports = {
   darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          yellow: "#FFDD33",
-          gray: "#98A2B3",
-        },
-        secondar: {
-          yellow: "#FFDD44",
-        },
-      },
-      fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+  				yellow: '#FFDD33',
+  				gray: '#98A2B3'
+  			},
+  			secondar: {
+  				yellow: '#FFDD44'
+  			}
+  		},
+  		fontFamily: {
+  			roboto: ["Roboto", "sans-serif"],
+  			poppins: ["Poppins", "sans-serif"]
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 };
