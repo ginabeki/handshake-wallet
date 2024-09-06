d460f34bcf9641af26c8fee4887083acfcae7c72
ginabeki <ginabeki3@gmail.com>
2024-09-06 21:48:49 +0300

    Render components to home page

--- src/pagesComponents/HomePage.tsx
+++ src/pagesComponents/HomePage.tsx
@@ -1,12 +1,14 @@
-import { Title } from "@/components";
 import HowItWorks from "@/components/howItWorks/HowItWorks";
 import React from "react";
-
+import SecurePurchases from "@/components/securePurchases/securePurchases";
+import Reviews from "@/components/reviews/reviews";
 const HomePage = () => {
   return (
     <main className="w-full mx-auto">
       <div className="w-full">
         <HowItWorks />
+        <SecurePurchases />
+        <Reviews />
       </div>
     </main>
   );
