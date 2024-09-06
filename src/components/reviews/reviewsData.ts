ff59aa81f2b48c9a1ac6265e3eb1d6e234ed2ba2
ginabeki <ginabeki3@gmail.com>
2024-09-06 21:47:33 +0300

    Create data for reviews section

---
+++ src/components/reviews/reviewsData.ts
@@ -0,0 +1,32 @@
+import { images } from "@/data";
+
+export const reviewsData = [
+  {
+    id: 1,
+    name: "John Doe",
+    img: images.icons.avataaars,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla non nunc scelerisque consectetur",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    img: images.icons.avataaars1,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla non nunc scelerisque consectetur",
+  },
+  {
+    id: 3,
+    name: "Jane Doe",
+    img: images.icons.avataaars,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla non nunc scelerisque consectetur",
+  },
+  {
+    id: 4,
+    name: "John Doe",
+    img: images.icons.avataaars1,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla non nunc scelerisque consectetur",
+  },
+];
