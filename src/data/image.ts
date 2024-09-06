be9e2ba2eeac1a009bbbb27f68edc02c83a64299
ginabeki <ginabeki3@gmail.com>
2024-09-06 21:48:32 +0300

    Add images

--- src/data/image.ts
+++ src/data/image.ts
@@ -1,8 +1,18 @@
 import logo from "../../public/images/logo/logo.png";
 import logo1 from "../../public/images/logo/logo1.png";
 import logo2 from "../../public/images/logo/logo2.png";
+import buyer from "../../public/images/icons/buyer.svg";
+import confirm from "../../public/images/icons/confirm.svg";
+import delivery from "../../public/images/icons/delivery.svg";
+import funds from "../../public/images/icons/funds.svg";
+import tracker from "../../public/images/icons/tracker.svg";
+import avataaars from "../../public/images/icons/avataaars.svg";
+import avataaars1 from "../../public/images/icons/avataaars1.svg";
+import newsletter from "../../public/images/icons/newsletter.svg";
+import footerLogo from "../../public/images/logo/footerLogo.svg";
 
-const logos = { logo, logo1, logo2 };
-const images = { ...logos };
+const logos = { logo, logo1, logo2, footerLogo };
+const icons = { buyer, confirm, delivery, funds, tracker, avataaars, avataaars1, newsletter };
+const images = { ...logos, icons };
 
 export { images };
