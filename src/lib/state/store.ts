e4e8531161401a404f2471cb93b3fe6ae1f57ade
ginabeki <ginabeki3@gmail.com>
2024-09-10 23:35:47 +0300

    Fix: add malware to fix non-serializable error

--- src/lib/state/store.ts
+++ src/lib/state/store.ts
@@ -5,6 +5,17 @@ export const store = configureStore({
   reducer: {
     auth: web5Reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ['auth/initializeweb5/fulfilled'],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
+        // Ignore these paths in the state
+        ignoredPaths: ['items.dates'],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
