4054d5506c409403ac035263ab04b5ebebdddc2a
ginabeki <ginabeki3@gmail.com>
2024-09-10 17:04:27 +0300

    Add web5 reducer in the store

--- src/lib/state/store.ts
+++ src/lib/state/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import web5Reducer from "./web5Slice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    web5: web5Reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
