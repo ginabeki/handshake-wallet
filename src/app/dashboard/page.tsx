f3717a5c18221950fd1659023f2750910ba43b63
ginabeki <ginabeki3@gmail.com>
2024-09-10 22:23:34 +0300

    Check if user is authenticated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+"use client";
+
+import { useAppSelector } from "@/lib/state/hooks";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
+    const router = useRouter();
+    const { isAuthenticated, did } = useAppSelector((state) => state.auth);
+    useEffect(() => {
+        if (!isAuthenticated && !did) {
+            router.push('/');
+        }
+    }, [isAuthenticated, did, router]);
     return (
         <div>dashboard page</div>
     )
